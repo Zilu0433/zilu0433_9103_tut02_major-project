9d48647fdc360ffead0a7bcdfe288bc55ffb7fc7
Ziniu Lu <167947545+Zilu0433@users.noreply.github.com>
2024-05-28 16:10:45 +1000

    add dots

--- sketch.js
+++ sketch.js
@@ -13,8 +13,6 @@ let y_offset_array = [];
 let length_offset_array = [];
 let rotate_offset_array = [];
 
-
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
@@ -51,82 +49,26 @@ function setup() {
 
   generateCurves(15,214,381,144,62,91,50,-3,5)
   generateCurves(16,214,379,226,84,126,55,-5,5)
-
-
-
-  // for(let i = 0; i < 10; i++) {
-  //   let random_number = int(random(5, 30)); 
-  //   number_array.push(random_number); 
-  // }
-  // for(let i = 0; i < 10; i++) {
-  //   let x_number = int(random(0, 490)); 
-  //   x_array.push(x_number); 
-  // }
-  // for(let i = 0; i < 10; i++) {
-  //   let y_number = int(random(0, 700)); 
-  //   y_array.push(y_number); 
-  // }
-  // for(let i = 0; i < 10; i++) {
-  //   let length_number = int(random(30, 150)); 
-  //   length_array.push(length_number); 
-  // }
-  // for(let i = 0; i < 10; i++) {
-  //   let rotate_number = int(random(-20, 20)); 
-  //   rotate_array.push(rotate_number); 
-  // }
-  // for(let i = 0; i < 10; i++) {
-  //   let PI_number = int(random(-2*PI, 2*PI)); 
-  //   PI_array.push(PI_number); 
-  // }
-
-
-  // for(let i = 0; i < 10; i++) {
-  //   let random_offset_number = int(random(-5, 5)); 
-  //   number_offset_array.push(random_offset_number); 
-  // }
-  // for(let i = 0; i < 10; i++) {
-  //   let x_offset_number = int(random(-5, 5)); 
-  //   x_offset_array.push(x_offset_number); 
-  // }
-  // for(let i = 0; i < 10; i++) {
-  //   let y_offset_number = int(random(-10, 10)); 
-  //   y_offset_array.push(y_offset_number); 
-  // }
-  // for(let i = 0; i < 10; i++) {
-  //   let length_offset_number = int(random(-10, 10)); 
-  //   length_offset_array.push(length_offset_number); 
-  // }
-  // for(let i = 0; i < 10; i++) {
-  //   let rotate_offset_number = int(random(-10, 10)); 
-  //   rotate_offset_array.push(rotate_offset_number); 
-  // }
-  
-  // for(let i = 0; i < 1; i++) {
-  //   generateCurves(number_array[i] + number_offset_array[i],x_array[i] + x_offset_array[i],y_array[i] + y_offset_array[i],144,62,91,length_array[i] + length_offset_array[i],rotate_array[i] + rotate_offset_array[i],PI_array[i]); 
-  //   generateCurves(number_array[i],x_array[i],y_array[i],226,84,126,length_array[i],rotate_array[i],PI_array[i]);
-    
-  //   print("generateCurves("+(number_array[i] + number_offset_array[i])+","+(x_array[i] + x_offset_array[i])+","+(y_array[i] + y_offset_array[i])+",144,62,91," +(length_array[i]+length_offset_array[i])+","+(rotate_array[i]+ rotate_offset_array[i])+","+ PI_array[i]+")");
-
-  
-
-  //   print("generateCurves("+(number_array[i])+","+(x_array[i])+","+(y_array[i])+",226,84,126," +(length_array[i])+","+(rotate_array[i])+","+ PI_array[i]+")");
-    
-    
-  // }
-
-
 }
 
 function draw() {
-  background(255,200,34);
+  background(255, 200, 34);
+
+  // Add black dots to the yellow background
+  for (let i = 0; i < 100; i++) { // Adjust the number of dots as needed
+    let x = random(width);
+    let y = random(height);
+    fill(0);
+    noStroke();
+    ellipse(x, y, 5, 5); // Adjust the size of the dots as needed
+  }
   
- 
   for (let i = 0; i < curves.length; i++) {
     curves[i].display();
   }
 }
 
-function Curve(startX, startY, controlX1, controlY1, controlX2, controlY2, endX, endY, r, g, b, size,rotate, round) {
+function Curve(startX, startY, controlX1, controlY1, controlX2, controlY2, endX, endY, r, g, b, size, rotate, round) {
   this.startX = startX;
   this.startY = startY;
   this.controlX1 = controlX1;
@@ -144,8 +86,6 @@ function Curve(startX, startY, controlX1, controlY1, controlX2, controlY2, endX,
   
   this.display = function() {
     stroke(this.r, this.g, this.b);
-    //226,84,126
-    //144,62,91
     strokeWeight(4.5); 
     noFill();
     beginShape();
@@ -155,10 +95,9 @@ function Curve(startX, startY, controlX1, controlY1, controlX2, controlY2, endX,
   };
 }
 
-
-function generateCurves(n,start_x,start_y,color_r, color_g, color_b, s, rotate_ratio, rr) {
+function generateCurves(n, start_x, start_y, color_r, color_g, color_b, s, rotate_ratio, rr) {
   let size = s;
-  let angleStep =  rr/ n;
+  let angleStep = rr / n;
   for (let i = 0; i < n; i++) {
     let startX = start_x;
     let startY = start_y;
